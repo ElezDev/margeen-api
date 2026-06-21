82a2373f8c2591f895a54c049a5603985c5d73cb
ElezDev <edwinledezma2013@gmail.com>
2026-06-20 19:01:29 -0500

    Update CORS configuration to allow all origins
    - Changed 'allowed_origins' from a filtered list based on environment variable to allow all origins by setting it to ['*']. This simplifies CORS handling for the application.

--- config/cors.php
+++ config/cors.php
@@ -6,8 +6,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter(explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173,http://127.0.0.1:5173'))),
-
+    //'allowed_origins' => array_filter(explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173,http://127.0.0.1:5173'))),
+    'allowed_origins' => ['*'], 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
